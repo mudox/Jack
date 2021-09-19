d90bfe75f9839a7c826b1559ccd17302c0b08fc3
mudox <imudox@gmail.com>
2021-09-19 20:47:01 +0800

    update testing action

--- .ap-actions/test-jack.zsh
+++ .ap-actions/test-jack.zsh
@@ -4,8 +4,15 @@ bin=~/Develop/Rust/jack/target/debug/jack
 
 $bin error    'Error message'
 $bin warn     'Warn message'
+$bin -b2 -a2 warn     'Warn message with 2 empty lines above and below'
 $bin info     'Info message'
 $bin debug    'Debug message'
 $bin verbose  'Verbose message'
 
 $bin success  'Success message'
+
+for i in {1..5}; do
+  $bin progress "[$i] In progress ..."
+  sleep 0.7
+done
+$bin success 'Done'
